06875d1967e1666883a697eb21e9a45a12c89154
Sidhu714 <cakewalk714@gmail.com>
2026-06-22 23:31:35 +0530

    otel test

--- src/otlp-parser.js
+++ src/otlp-parser.js
@@ -80,3 +80,39 @@ function toHex(value){
 
     
 }
+
+
+
+
+// const body = {
+//   resourceSpans: [
+//     {
+//       resource: {
+//         attributes: [
+//           {
+//             key: "service.name",
+//             value: { stringValue: "my-app" }
+//           }
+//         ]
+//       },
+//       scopeSpans: [
+//         {
+//           spans: [
+//             {
+//               traceId: "abc123",
+//               spanId: "111",
+//               parentSpanId: "",
+//               name: "GET /users",
+//               startTimeUnixNano: "1000",
+//               endTimeUnixNano: "2000",
+//               attributes: [],
+//               status: { code: 0 },
+//               events: []
+//             }
+//           ]
+//         }
+//       ]
+//     }
+//   ]
+// };
+
---
+++ src/playground/otlp.js
@@ -0,0 +1,37 @@
+import { parseOtlpJson } from "../otlp-parser.js";
+
+
+const body = {
+  resourceSpans: [
+    {
+      resource: {
+        attributes: [
+          {
+            key: "service.name",
+            value: { stringValue: "my-app" }
+          }
+        ]
+      },
+      scopeSpans: [
+        {
+          spans: [
+            {
+              traceId: "abc123",
+              spanId: "111",
+              parentSpanId: "",
+              name: "GET /users",
+              startTimeUnixNano: "1000",
+              endTimeUnixNano: "2000",
+              attributes: [],
+              status: { code: 0 },
+              events: []
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+
+console.log(parseOtlpJson(JSON.stringify(body)));
